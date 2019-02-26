bc2e7533c27fda0496288c7ae4813e54beaa8aa1
nbthanh98 <nguyenbathanh1280@gmail.com>
2019-02-26 22:40:37 +0700

    TH-SuDungToanTu

---
+++ TH-SuDungToanTu/index.js
@@ -0,0 +1,9 @@
+let inputWidth;
+let inputHeight;
+
+inputWidth = prompt('Nhap vao Chieu rong: ');
+inputHeight = prompt('Nhap vao Chieu Cao: ');
+
+let area = inputHeight * inputWidth;
+
+document.write('area = ' + area);
